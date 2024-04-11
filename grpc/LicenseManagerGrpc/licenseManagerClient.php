2838b80a1f61107d8740fba30a16787dd9a4912e
getcompass-opensource <opensource@getcompass.ru>
2024-04-11 17:02:06 +0400

    Release v2.0.0

---
+++ grpc/LicenseManagerGrpc/licenseManagerClient.php
@@ -0,0 +1,20 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace LicenseManagerGrpc;
+
+/**
+ * сервис, который описывает все методы go_license_manager
+ */
+class licenseManagerClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+}
